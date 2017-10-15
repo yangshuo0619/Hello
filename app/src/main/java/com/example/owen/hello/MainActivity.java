7dd9c79777156a63ad292a9de496c7ab0c67da75
yangshuo0619 <1925107123@qq.com>
2017-10-15 16:51:10 +0800

    add start button

--- app/src/main/java/com/example/owen/hello/MainActivity.java
+++ app/src/main/java/com/example/owen/hello/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.owen.hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = (Button) findViewById(R.id.btn_start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"hello world",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
